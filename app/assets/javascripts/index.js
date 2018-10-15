b624f79d48458e1df676a94af0d6c1f3bd329be5
uehara55 <yusukeuehara55@gmail.com>
2018-10-15 19:31:55 +0900

    Modified search

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -21,23 +21,25 @@ $(function() {
 
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
-    $.ajax({
-      type: "GET",
-      url: "/users",
-      data: {keyword: input},
-      dataType: "json",
-    })
-    .done(function(users) {
-      $(".user-search-result").empty();
-      if (users.length !== 0) {
-        users.forEach(function(user){
-          appendUser(user);
-        });
-      }
-    })
-    .fail(function() {
-      alert('ユーザー検索に失敗しました');
-    })
+    if (input.length !== 0) {
+      $.ajax({
+        type: "GET",
+        url: "/users",
+        data: {keyword: input},
+        dataType: "json",
+      })
+      .done(function(users) {
+        $(".user-search-result").empty();
+        if (users.length !== 0) {
+          users.forEach(function(user){
+            appendUser(user);
+          });
+        }
+      })
+      .fail(function() {
+        alert('ユーザー検索に失敗しました');
+      })
+    }
   });
   $(document).on("click", ".chat-group-user__btn--add", function(){
     var userId = $(this).attr("data-user-id");
